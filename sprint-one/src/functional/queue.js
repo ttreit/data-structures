5a0a9426ab93acef308049b1876ec5cf737a91e2
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-07 14:38:49 -0700

    Create functional queue constructor

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,18 +2,59 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  var storage = someInstance.storage;
+  var i = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[i] = value;
+    i ++;
   };
 
   someInstance.dequeue = function() {
+    var result = storage['0'];
+    delete storage['0'];
+    if (i > 0) {
+      i--;
+    }
+    for (var key in storage) {
+      var newKey = parseInt(key) - 1;
+      var oldKey = key; //for readability
+      storage[newKey] = storage[oldKey];
+      delete storage[oldKey];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return i;
   };
 
   return someInstance;
 };
+
+//Pseudocode
+
+// create an empty storage object
+
+// set i = 0
+
+// ## enqueue (string)
+// push k/v pair (i: string) into storage object
+
+// increment i
+
+// ## dequeue()
+// return value with key '0' (create seperate function for this)
+
+// delete k/v pair at key i
+
+// decrement i
+
+// derement all keys
+
+// ## size()
+// return i
+
