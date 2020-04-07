35b34bd369bab4ba73af5944cb26a232c7a7f655
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-07 16:25:38 -0700

    Complete functional stack and queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,51 @@
 var Stack = function() {
   var someInstance = {};
+  var i = 0;
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {}; //allows access to storage from maker function
+  var storage = someInstance.storage; //readibility in later code
+
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[i] = value;
+    i++;
   };
 
   someInstance.pop = function() {
+    if (i >0) {
+      i--;
+    }
+    var result = storage[i];
+    delete storage[i];
+    return result;
   };
 
   someInstance.size = function() {
+    return i;
   };
 
   return someInstance;
 };
+
+//Pseudocode
+
+// set i = 0
+
+// declare stoarge object
+
+// ## push(string)
+// add {i: string} to storage object
+
+// increment i
+
+// ## pop()
+// decrement i
+
+// return {i: string}
+
+// delete (i: string)
+
+// ## size()
+// return i
