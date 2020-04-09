94bab03d40607a5b4e9122385054ba2662341f20
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-08 20:49:47 -0700

    Complete functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,60 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.i = 0;
+  // get methods into constructor
+  for (key in queueMethods) {
+    queue[key] = queueMethods[key];
+  }
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.i] = value;
+    this.i ++;
+  },
+  dequeue: function() {
+    var dqValue = this.storage['0'];
+    delete this.storage['0'];
+    if (this.i > 0) {
+      this.i--;
+    }
+    for (key in this.storage) {
+      var newKey = parseInt(key) - 1;
+      var oldKey = key;
+      this.storage[newKey] = this.storage[oldKey];
+      delete this.storage[oldKey];
+    }
+    return dqValue;
+  },
+  size: function() {
+    return this.i;
+  }
+};
 
 
+// pseudocode
+// create an empty storage object
+
+// set i = 0
+
+// ## enqueue (string)
+// push k/v pair (i: string) into storage object
+
+// increment i
+
+// ## dequeue()
+// return value with key '0' (create seperate function for this)
+
+// delete k/v pair at key i
+
+// decrement i
+
+// derement all keys
+
+// ## size()
+// return i
+
+//***SCRAPS***
