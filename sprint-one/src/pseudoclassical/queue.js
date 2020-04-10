38f7e81f7d9f904a6e4a0832b347988fc2aa6a6e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-09 18:27:14 -0700

    Create pseudoclassical queue - one bug to fix
    .prototype constructor property not pointing back to the constructor
    itself.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,50 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.i = 0;
+};
+
+Queue.prototype = {
+  enqueue: function(value) {
+    this.storage[this.i] = value;
+    this.i++;
+  },
+  dequeue: function() {
+    var dqResult = this.storage['0'];
+    delete this.storage['0'];
+    if (this.i > 0) {
+      this.i--;
+    }
+    for (key in this.storage) {
+      var newKey = parseInt(key) - 1;
+      var oldKey = key;
+      this.storage[newKey] = this.storage[oldKey];
+      delete this.storage[oldKey];
+    }
+    return dqResult;
+  },
+
+  size: function() {
+    return this.i;
+
+  }
+
 };
 
 
+// create an empty storage object
+
+// set i = 0
+
+// ## enqueue (string)
+// push k/v pair (i: string) into storage object
+
+// increment i
+
+// ## dequeue()
+// return value with key '0' (create seperate function for this)
+
+// delete k/v pair at key 0
+
+// decrement i
+
+// derement all keys
