9fbd7234be64ffb5d69d94dca2046f0e67926ad0
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-09 18:50:12 -0700

    Fix bug - missing constructor property

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,48 +3,26 @@ var Queue = function() {
   this.i = 0;
 };
 
-Queue.prototype = {
-  enqueue: function(value) {
-    this.storage[this.i] = value;
-    this.i++;
-  },
-  dequeue: function() {
-    var dqResult = this.storage['0'];
-    delete this.storage['0'];
-    if (this.i > 0) {
-      this.i--;
-    }
-    for (key in this.storage) {
-      var newKey = parseInt(key) - 1;
-      var oldKey = key;
-      this.storage[newKey] = this.storage[oldKey];
-      delete this.storage[oldKey];
-    }
-    return dqResult;
-  },
-
-  size: function() {
-    return this.i;
-
-  }
-
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.i] = value;
+  this.i++;
 };
 
+Queue.prototype.dequeue = function() {
+  var dqResult = this.storage['0'];
+  delete this.storage['0'];
+  if (this.i > 0) {
+    this.i--;
+  }
+  for (key in this.storage) {
+    var newKey = parseInt(key) - 1;
+    var oldKey = key;
+    this.storage[newKey] = this.storage[oldKey];
+    delete this.storage[oldKey];
+  }
+  return dqResult;
+};
 
-// create an empty storage object
-
-// set i = 0
-
-// ## enqueue (string)
-// push k/v pair (i: string) into storage object
-
-// increment i
-
-// ## dequeue()
-// return value with key '0' (create seperate function for this)
-
-// delete k/v pair at key 0
-
-// decrement i
-
-// derement all keys
+Queue.prototype.size = function() {
+  return this.i;
+};
