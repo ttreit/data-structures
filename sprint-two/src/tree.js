c741e767091f434a889b4b5ef7bd4c8ad1f4b3e3
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-14 13:59:41 -0700

    Work on tree function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = null; // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,12 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+
 };
 
 treeMethods.contains = function(target) {
+
 };
 
 
