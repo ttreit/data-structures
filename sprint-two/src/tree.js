dda5048607bba1871bbfa681019b129ac977560e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-14 19:25:48 -0700

    Create initial tree function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,38 @@
 var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
-
-  // your code here
-  newTree.children = null; // fix me
-
-  _.extend(newTree, treeMethods);
-
-  return newTree;
+  var node = {};
+  node.value = value;
+  node.children = [];
+  _.extend(node, treeMethods);
+  return node;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-
-
+  var childNode = Tree(value);
+  this.children.push(childNode);
 };
 
 treeMethods.contains = function(target) {
+  var findTarget = function(children, target) { //children = node.children (an array)
+    if (children.length === 0) {
+      return false;
+    } else {
+      for (var i = 0; i < children.length; i++) {
+        if (children[i].value === target) {
+          return true;
+        } else {
+          findTarget(children[i], target);
+        }
+      }
+    }
+  };
 
+  if (this.value === target) {
+    return true;
+  } else {
+    return findTarget(this.children, target);
+  }
 };
 
 
@@ -26,3 +40,4 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
