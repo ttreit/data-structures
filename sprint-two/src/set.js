ecd622edb25ba8e4cc2e59e1fbb117b59c6ef3f9
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-18 12:18:46 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,22 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fixed!
   return set;
 };
 
 var setPrototype = {};
 
+// takes any string and adds it to the set
 setPrototype.add = function(item) {
+  this._storage[item] = null;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
