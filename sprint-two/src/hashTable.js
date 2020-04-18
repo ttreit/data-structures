3e4f2245b7f75c7c1e29bf0f987f15bfc8accc2a
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-18 08:53:26 -0700

    In-process

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,37 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  // this.count = 0;
+  // this.maxSize = Math.floor(this.limit * 0.75);
+  // if (this.count > this.maxSize) {
+  //   this.count = 0;
+  //   this.storage.each(callback);
+  // }
+  // var callback = function(item, index, storage) {
+  //   //get item
+  //   //delete item from previous location
+  //   //re-hash item and put back in storage
+
+
+  // };
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this.count += 1;
+  this._storage.set(index, value);
+
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  return this._storage.get(index);
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  debugger;
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var toDelete = this.storage.get(index);
 };
 
 
